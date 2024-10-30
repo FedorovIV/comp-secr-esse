bca416ac1261b2473bc5ac0f229bad9c271b4edf
FedorovIV <fedorov.iv@phystech.edu>
2024-10-30 12:40:00 +0300

    add new feature

--- source/pki/https/server.js
+++ source/pki/https/server.js
@@ -1,22 +1,42 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
+const session = require("express-session");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../dist")));
+
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+
+app.get("/testSession", (req, res)=>{
+  if (req.session.views) {
+    req.session.views++;
+    res.send(`Вы посетили эту страницу ${req.session.views} раз`);
+  } else {
+    req.session.views = 1;
+    res.send('Добро пожаловать! Это ваш первый визит.');
+  }
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
 const options = {
-  pfx: fs.readFileSync(path.join(__dirname,'comp-secr-esse.pfx')),
+  pfx: fs.readFileSync(path.join(__dirname, "comp-secr-esse.pfx")),
   passphrase: "EB6D471DBB32B9F9BB3D98E86421BDC76102EA47",
 };
 
 https.createServer(options, app).listen(443, () => {
-  console.log('HTTPS сервер запущен на порту 443');
+  console.log("HTTPS сервер запущен на порту 443");
 });
 
